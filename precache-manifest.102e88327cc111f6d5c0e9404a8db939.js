5a98656e8f9d291f7e4eac1a55048ed0f520edcc
sidhant <sidhantsharma293@gmail.com>
2020-06-13 17:21:06 +0530

    Updates

---
+++ precache-manifest.102e88327cc111f6d5c0e9404a8db939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564f06ee90c19486dab0284253c819fb",
+    "url": "/Typing-Speed-Calculator/index.html"
+  },
+  {
+    "revision": "740259ea48640fc44ad6",
+    "url": "/Typing-Speed-Calculator/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "377a353849ae74d52ecd",
+    "url": "/Typing-Speed-Calculator/static/css/main.19f4448b.chunk.css"
+  },
+  {
+    "revision": "740259ea48640fc44ad6",
+    "url": "/Typing-Speed-Calculator/static/js/2.f68e34ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Typing-Speed-Calculator/static/js/2.f68e34ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377a353849ae74d52ecd",
+    "url": "/Typing-Speed-Calculator/static/js/main.81045ab1.chunk.js"
+  },
+  {
+    "revision": "03c5a0e53ca177c89d6b",
+    "url": "/Typing-Speed-Calculator/static/js/runtime-main.7ea80414.js"
+  }
+]);
